3e725aeba833e886fc0446d7745c21619c8e2718
Damian <damian.lewandowsky@gmail.com>
2022-04-08 16:46:12 +0200

    Dodaj pliki projektów.

---
+++ FolderWatcher/Db_Connector.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace FolderWatcher
+{
+    public class Db_Connector
+    {
+        public SqliteConnection sqlite_conn { get; set; }
+        public Db_Connector()
+        {
+           // SqliteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            CreateTable(sqlite_conn);
+            //InsertData(sqlite_conn);
+            //ReadData(sqlite_conn);
+        }
+        public SqliteConnection CreateConnection()
+        {
+           // SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            //SqliteConnection.CreateFile(@"C:\Users\DQL\Downloads\Test Excel\Nowy folder\synccc.sqlite");
+
+            SqliteConnection sqlite_conn;
+           // sqlite_conn.cre
+            // Create a new database connection:
+            sqlite_conn = new SqliteConnection("Data Source= SQLITE.db;  ");
+
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        public void CreateTable(SqliteConnection conn)
+        {
+
+            SqliteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE logs  (State VARCHAR(225),FilePath VARCHAR(225), Data VARCHAR(225))";
+
+           // string Createsql1 = "CREATE TABLE SampleTable1 (Col1 VARCHAR(20), Col2 INT)";
+
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            try
+            {
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {  }
+           
+           // sqlite_cmd.CommandText = Createsql1;
+           // sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+        public void InsertData(string state, string path, string data)
+        {
+            SqliteCommand sqlite_cmd;
+            //sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd = this.sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = $"INSERT INTO logs  (State, FilePath, Data) VALUES('{state}', '{path}','{data}'); "; //VALUES('Test Text ', 1); 
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        public void ReadData()
+        {
+            SqliteDataReader sqlite_datareader;
+            SqliteCommand sqlite_cmd;
+            sqlite_cmd = this.sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM logs";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                string myreader1 = sqlite_datareader.GetString(1);
+                string myreader2 = sqlite_datareader.GetString(2);
+                Console.WriteLine($"{myreader}  {myreader1} {myreader2}");
+            }
+            this.sqlite_conn.Close();
+        }
+
+    }
+}
---
+++ FolderWatcher/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace FolderWatcher
+{
+    class Program
+    {
+        public static Db_Connector connector { get; set; }
+        static void Main(string[] args)
+        {
+            connector = new Db_Connector();
+            Console.Write("Set folder path => ");
+            string path = Console.ReadLine();
+
+            using var watcher = new FileSystemWatcher(path);
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+           //watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Error += OnError;
+
+            //  watcher.Filter = "*.txt";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Już działm");
+            Console.ReadLine();
+
+            Console.WriteLine("---------------------------");
+            connector.ReadData();
+            Console.WriteLine("---------------------------");
+
+
+        }
+
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            connector.InsertData($"Changed", e.FullPath, DateTime.UtcNow.ToString());
+            Console.WriteLine($"Changed: {e.FullPath}   " + DateTime.UtcNow);
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string value = $"Created: {e.FullPath}  " + DateTime.UtcNow;
+            connector.InsertData($"Created", e.FullPath, DateTime.UtcNow.ToString());
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            connector.InsertData($"Deleted", e.FullPath, DateTime.UtcNow.ToString());
+            Console.WriteLine($"Deleted: {e.FullPath}   " + DateTime.UtcNow);
+            Console.ResetColor();
+        }
+
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+            Console.ResetColor();
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}   " + DateTime.UtcNow);
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+
+
+    }
+}
